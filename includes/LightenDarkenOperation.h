55f94e7d4fc188691d1604f499654b35f70fbaf8
Boreke <hippomartin03@gmail.com>
2025-04-28 12:04:58 +0200

    Lighten Darken operations handling contrast and brightness complete

--- includes/LightenDarkenOperation.h
+++ includes/LightenDarkenOperation.h
@@ -4,10 +4,18 @@
 
 #ifndef LIGHTENDARKENOPERATION_H
 #define LIGHTENDARKENOPERATION_H
+#include <opencv2/opencv.hpp>
 
-
-
+using namespace std;
+using namespace cv;
 class LightenDarkenOperation {
+public:
+    // Constructor
+    LightenDarkenOperation() = default;
+    // Destructor
+    ~LightenDarkenOperation() = default;
+    // Method to apply the operation
+    void apply(Mat& image);
 
 };
 
--- src/LightenDarkenOperation.cpp
+++ src/LightenDarkenOperation.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "LightenDarkenOperation.h"
+#include <opencv2/opencv.hpp>
+
+void LightenDarkenOperation::apply(Mat& image) {
+    // Check if the image is empty
+    if (image.empty()) {
+        cout << "Image is empty" << endl;
+        return;
+    }
+    cout << "Enter alpha value: " << endl;
+    double alpha;
+    cin >> alpha;
+    cout << "Enter beta value: " << endl;
+    double beta;
+    cin >> beta;
+    // Create a new image to store the result
+    Mat newImage = Mat::zeros(image.size(), image.type());
+
+    // Apply the lighten/darken operation
+    for (int y = 0; y < image.rows; y++) {
+        for (int x = 0; x < image.cols; x++) {
+            for (int c = 0; c < 3; c++) {
+                newImage.at<Vec3b>(y, x)[c] =
+                    saturate_cast<uchar>(alpha * image.at<Vec3b>(y, x)[c] + beta);
+            }
+        }
+    }
+    image = newImage;
+}
