034db9a5795c0bf298a44701240ab2679872037d
Boreke <hippomartin03@gmail.com>
2025-04-28 11:19:48 +0200

    Operation virtual class complete

--- includes/Operation.h
+++ includes/Operation.h
@@ -4,13 +4,15 @@
 
 #ifndef OPERATION_H
 #define OPERATION_H
+#include <opencv2/opencv.hpp>
 
-
+using namespace cv;
+using namespace std;
 
 class Operation {
-
+    public:
+      virtual void apply(Mat& image) = 0;
+      virtual ~Operation() {}
 };
-
-
 
 #endif //OPERATION_H
