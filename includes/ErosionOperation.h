2c25457de7ea7a2cd1fd72e3b81e72cc42e39757
Zoomma1 <victorgerm1@hotmail.fr>
2025-04-29 09:24:57 +0200

    Implement Dilation and Erosion operations with user input for size and added user input in main

--- includes/ErosionOperation.h
+++ includes/ErosionOperation.h
@@ -4,11 +4,13 @@
 
 #ifndef EROSIONOPERATION_H
 #define EROSIONOPERATION_H
+#include <Operation.h>
+#include <opencv2/opencv.hpp>
 
 
-
-class ErosionOperation {
-
+class ErosionOperation final: public Operation{
+public:
+    void apply(Mat &image) override;
 };
 
 
--- src/ErosionOperation.cpp
+++ src/ErosionOperation.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "ErosionOperation.h"
+
+void ErosionOperation::apply(Mat &image) {
+    if ( image.empty() ) {
+        cerr << "Could not open or find the image" << endl;
+        return;
+    }
+    int erosionSize;
+    cout << "Enter the erosion size (positive integer): ";
+    while (!(cin >> erosionSize) || erosionSize < 0) {
+        cout << "Invalid input. Please enter a positive integer for erosion size: ";
+        cin.clear();
+    }
+
+    const Mat element = getStructuringElement(MORPH_RECT,
+                                         Size(2 * erosionSize + 1, 2 * erosionSize + 1),
+                                         Point(erosionSize, erosionSize));
+    erode(image, image, element);
+}
